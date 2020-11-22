08fe62d62e8262a7c2afb17e738dd5814f6ae8bc
jeky22 <qhzh33@gmail.com>
2020-11-23 03:11:49 +0900

    build bootstrap-env

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 import env from '../static/settings-local'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
