5c3d53bd94636c74242e6adf539f72d5c96e6df9
jeky22 <qhzh33@gmail.com>
2020-11-23 03:12:41 +0900

    decorate login page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,91 +1,16 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-     <button @click="onClickSendData">
-      Send Data
-    </button>
-      <router-link :to="{ path: routerTestURL }">
-        이동
-      </router-link>
+    <h1>LOGIN PAGE</h1>
+    <div class="col-12 justify-content-center">
+      <div class="col-12 mt-3">
+        <input placeholder="ID" class="col-9" type="text" name="id" v-model="id" />
+      </div>
+      <div class="col-12 mt-3 ">
+        <input placeholder="Password" class="col-9" type="password" name="password" v-model="password" />
+      </div>
+    </div>
+    <button class="btn btn-success" :disabled="!id&&!password" @click="onClickSendData">로그인</button>
+    <router-link class="btn border-success" :to="{ path: routerTestURL }">회원가입</router-link>
   </div>
 </template>
 
@@ -96,7 +21,8 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      id: 'hi',
+      password: 'pw'
     }
   },
   methods: {
@@ -105,7 +31,11 @@ export default {
         email: 'test@test.test',
         gender: 'male'
       }
-      let res = await httpRequest.httpTest(this.$http, this.$env.apiUrl, data).catch((e) => { console.log(e) })
+      let res = await httpRequest
+        .httpTest(this.$http, this.$env.apiUrl, data)
+        .catch(e => {
+          console.log(e)
+        })
       console.log(res.data['email'])
       console.log(res.data['gender'])
     }
@@ -120,7 +50,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
