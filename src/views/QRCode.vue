a774fc340f7d904483aec2db6944bc5648d380e9
jaypae95 <jaypae95@gmail.com>
2020-12-02 22:55:06 -0800

    qrcode sample

--- src/views/QRCode.vue
+++ src/views/QRCode.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>
-        qrcode
-    </div>
+  <div>
+     <vue-qrcode v-bind:value="transferUrl" />
+  </div>
 </template>
+
+<script>
+import VueQrcode from 'vue-qrcode'
+export default {
+  data () {
+    return {
+      transferUrl: 'naver.com'
+    }
+  },
+  components: {
+    VueQrcode
+  }
+}
+</script>
