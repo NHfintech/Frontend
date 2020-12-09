57e3515a3299b4c115a63af305df36b3579a31c6
jaypae95 <jaypae95@gmail.com>
2020-12-08 23:34:52 -0800

    restore logout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,17 @@
         <b-nav-item href="/">이벤트</b-nav-item>
         <b-nav-item href="/breakdown">송금 내역</b-nav-item>
       </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>{{ $store.state.user.id }}</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item @click="logout()">logout</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
     </b-navbar>
     <router-view></router-view>
   </div>
@@ -12,11 +23,12 @@
 
 <script>
 export default {
-  name: 'HelloWorld'
-  // methods: {
-  //   g () {
-  //     console.log
-  //   }
-  // }
+  name: 'HelloWorld',
+  methods: {
+    logout () {
+      this.$store.commit('removeUser')
+      this.$router.replace({ path: '/login' }).catch(() => {})
+    }
+  }
 }
 </script>
